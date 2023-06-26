4d7d8afbfb34a8ae15257ab51261bc56456980a7
developersung13 <developersung13@gmail.com>
2023-06-26 19:30:41 +0900

    [fix]: prettier 설정 변경으로 인한 기존 파일 내 세미콜론 문자 추가 및 반응형 웹 디자인 로직 추가 (#7)

--- app/components/Contest.tsx
+++ app/components/Contest.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 export default function Contest() {
   return (
@@ -9,12 +9,12 @@ export default function Contest() {
           제2회 충청북도 대학생 프로그래밍 경진대회 예선 [한국교통대학교 5/10]
         </Link>
       </p>
-      <div>
+      <div className="flex flex-col 3xs:inline-block">
         <span className="text-xs">
           <span className="font-semibold">신청 기간</span> : ~{' '}
           <span className="font-light">2023.06.26. 03:00</span>
         </span>
-        <span className="before:content-['|'] mx-3 font-thin text-[#aaa]"></span>
+        <span className="hidden 3xs:inline-block before:content-['|'] mx-3 font-thin text-[#aaa]"></span>
         <span className="text-xs">
           <span className="font-semibold">대회 시작</span> :{' '}
           <span className=""> 2023.06.26. 06:00</span>
@@ -22,5 +22,5 @@ export default function Contest() {
       </div>
       <div className="absolute right-0 bottom-0 border-l-[0.6rem] border-l-[#eee] border-t-[0.6rem] border-t-[#eee] border-b-[0.6rem] border-b-white border-r-[0.6rem] border-r-white group-hover:border-l-[#3274ba] group-hover:border-t-[#3274ba] ease-in duration-100"></div>
     </div>
-  )
+  );
 }
