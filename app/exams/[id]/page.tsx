842a1eed98912102791d1c470c24980c9dd5f821
developersung13 <developersung13@gmail.com>
2024-01-02 10:23:54 +0900

    [fix]: confirm 함수에 대한 변수 선언 키워드 일괄 변경 (#80)

--- app/exams/[id]/page.tsx
+++ app/exams/[id]/page.tsx
@@ -16,7 +16,7 @@ export default function ExamDetail() {
   const router = useRouter();
 
   const handleDeleteExam = () => {
-    let userResponse = confirm(
+    const userResponse = confirm(
       '현재 시험 게시글을 삭제하시겠습니까?\n삭제 후 내용을 되돌릴 수 없습니다.',
     );
     if (!userResponse) return;
