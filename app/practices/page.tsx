e7edcc846f69c50c57240782604ecb397662a1b6
developersung13 <developersung13@gmail.com>
2024-01-17 13:35:23 +0900

    [feat]: {연습문제 목록} 페이지 상단 UI 변경 (#121)

--- app/practices/page.tsx
+++ app/practices/page.tsx
@@ -6,7 +6,15 @@ export default function Practices() {
   return (
     <div className="mt-2 px-5 2lg:px-0 overflow-x-auto">
       <div className="flex flex-col w-[60rem] mx-auto">
-        <p className="text-2xl font-semibold">📚 연습문제 목록</p>
+        <p className="flex items-center text-3xl font-semibold tracking-wide">
+          <img
+            src="/images/practice.png"
+            alt="exam"
+            style={{ width: '5rem' }}
+            className="ml-[-1rem] drop-shadow-md scale-90 fade-in-fast"
+          />
+          <span className="ml-3 lift-up">연습문제 목록</span>
+        </p>
         <form className="mt-5 mb-4">
           <div className="flex">
             <div className="flex flex-col relative z-0 w-1/2 group">
