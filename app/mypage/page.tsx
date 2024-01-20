407adfeedd58d400a4ba7edc879203d448d2293d
developersung13 <developersung13@gmail.com>
2024-01-20 23:27:42 +0900

    [feat]: {마이페이지} 컴포넌트 추가 (#133)

---
+++ app/mypage/page.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import Loading from '../loading';
+// import PostList from './PostList';
+import { useRouter } from 'next/navigation';
+import ProfileInformation from './components/ProfileInformation';
+import ParticipationHistory from './components/ParticipationHistory/ParticipationHistory';
+import ManagingPost from './components/ManagingPost/ManagingPost';
+
+export default function Mypage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tab, setTab] = useState('profileInformation');
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
+  const handleChangeTab = (tabName: string) => {
+    setTab(tabName);
+  };
+
+  if (isLoading) return <Loading />;
+
+  return (
+    <div className="mt-6 px-5 2lg:px-0 overflow-x-auto">
+      <div className="flex flex-col w-[60rem] mx-auto">
+        <div className="text-3xl font-semibold tracking-wide lift-up">
+          마이페이지
+        </div>
+        <div className="flex gap-8 mt-10">
+          <div className="w-52">
+            <div className="flex flex-col items-start gap-[1.125rem] font-semibold tracking-wide">
+              <button
+                onClick={() => handleChangeTab('profileInformation')}
+                className={`${
+                  tab === 'profileInformation'
+                    ? 'text-[#242424] font-bold'
+                    : 'text-[#6e6e6e] hover:text-[#3a8af9]'
+                } `}
+              >
+                프로필 정보
+              </button>
+              <button
+                onClick={() => handleChangeTab('participationHistory')}
+                className={`${
+                  tab === 'participationHistory'
+                    ? 'text-[#242424] font-bold'
+                    : 'text-[#6e6e6e] hover:text-[#3a8af9]'
+                } `}
+              >
+                참가 내역
+              </button>
+              <button
+                onClick={() => handleChangeTab('managingPost')}
+                className={`${
+                  tab === 'managingPost'
+                    ? 'text-[#242424] font-bold'
+                    : 'text-[#6e6e6e] hover:text-[#3a8af9]'
+                } `}
+              >
+                작성글 관리
+              </button>
+            </div>
+          </div>
+
+          <div className="w-full mt-[-0.75rem]">
+            {tab === 'profileInformation' ? (
+              <ProfileInformation />
+            ) : tab === 'participationHistory' ? (
+              <ParticipationHistory />
+            ) : (
+              <ManagingPost />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
