afa6ad59f037923652d4381b9d1b202a9a478787
developersung13 <developersung13@gmail.com>
2024-01-21 09:22:48 +0900

    [feat]: 시험 컴포넌트 문구 정정 (#133)

--- app/components/Exam.tsx
+++ app/components/Exam.tsx
@@ -11,7 +11,7 @@ export default function exam() {
       </p>
       <div>
         <span className="text-xs">
-          <span className="font-semibold">진행 기간</span> :{' '}
+          <span className="font-semibold">시험 시간</span> :{' '}
           <span className="font-light">
             2023.06.26. 03:00 ~ 2023.06.26. 03:00
           </span>
@@ -21,7 +21,7 @@ export default function exam() {
         <span className="text-xs font-semibold text-blue-600">#신재혁</span>
         <span className="before:content-['|'] mx-3 font-thin text-[#aaa]"></span>
         <span className="text-xs font-semibold">
-          EASY코스] 파이썬 프로그래밍
+          EASY코스 파이썬 프로그래밍
         </span>
       </div>
       <div className="absolute right-0 bottom-0 border-l-[0.6rem] border-l-[#eee] border-t-[0.6rem] border-t-[#eee] border-b-[0.6rem] border-b-white border-r-[0.6rem] border-r-white group-hover:border-l-[#3274ba] group-hover:border-t-[#3274ba] ease-in duration-100"></div>
