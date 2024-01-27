8457b040c53b83e137c50c18b245da6f5fe58b16
developersung13 <developersung13@gmail.com>
2024-01-27 16:26:06 +0900

    [feat]: 컴포넌트 리스트 사용 및 {신청 가능한 대회/추가된 시험} 요소 위치 변경 (#147)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import Contest from './components/Contest';
-import Exam from './components/Exam';
+import ContestList from './components/Contests/ContestList';
+import ExamList from './components/exams/ExamList';
 
 export default function Home() {
   return (
@@ -13,26 +13,6 @@ export default function Home() {
 
       <div className="flex">
         <div className="flex-col 2lg:flex-row mx-auto mt-12 mb-10 flex justify-center gap-5">
-          <div className="w-[22.5rem] 3xs:w-[30rem] p-2 mb-8 2lg:mb-0">
-            <div className="mb-5">
-              <Link
-                href="/contests"
-                className="text-[#595f68] text-xl pb-[11px] border-b-2 border-[#3274ba] hover:text-black focus:text-black"
-              >
-                신청 가능한 대회
-              </Link>
-              <div className="pb-2 border-b-[1.5px] border-dotted"></div>
-            </div>
-
-            <div className="flex flex-col gap-3">
-              {/* 데이터 임시로 추가 */}
-              <Contest />
-              <Contest />
-              <Contest />
-              <Contest />
-            </div>
-          </div>
-
           <div className="w-[22.5rem] 3xs:w-[30rem] p-2 ">
             <div className="mb-5">
               <Link
@@ -45,34 +25,23 @@ export default function Home() {
             </div>
 
             <div className="flex flex-col gap-3">
-              {/* 데이터 임시로 추가 */}
-              <Exam />
-              <Exam />
-              <div className="relative flex flex-col gap-4 bg-[#f7f7f7] p-3 group">
-                <p className="font-bold">
-                  <Link href="/" className="hover:underline">
-                    코딩테스트 3차
-                  </Link>
-                </p>
-                <div>
-                  <span className="text-xs font-semibold">
-                    시험 시간 :{' '}
-                    <span className="font-light">
-                      2023.06.26. 03:00 ~ 2023.06.26. 03:00
-                    </span>
-                  </span>
-                </div>
-                <div className="mt-[-0.9rem]">
-                  <span className="text-xs font-semibold text-blue-600">
-                    #노서영
-                  </span>
-                  <span className="before:content-['|'] mx-3 font-thin text-[#aaa]"></span>
-                  <span className="text-xs font-semibold">
-                    2023-01-자료구조(소프트웨어학부 01반){' '}
-                  </span>
-                </div>
-                <div className="absolute right-0 bottom-0 border-l-[0.6rem] border-l-[#eee] border-t-[0.6rem] border-t-[#eee] border-b-[0.6rem] border-b-white border-r-[0.6rem] border-r-white group-hover:border-l-[#3274ba] group-hover:border-t-[#3274ba] ease-in duration-100"></div>
-              </div>
+              <ExamList />
+            </div>
+          </div>
+
+          <div className="w-[22.5rem] 3xs:w-[30rem] p-2 mb-8 2lg:mb-0">
+            <div className="mb-5">
+              <Link
+                href="/contests"
+                className="text-[#595f68] text-xl pb-[11px] border-b-2 border-[#3274ba] hover:text-black focus:text-black"
+              >
+                신청 가능한 대회
+              </Link>
+              <div className="pb-2 border-b-[1.5px] border-dotted"></div>
+            </div>
+
+            <div className="flex flex-col gap-3">
+              <ContestList />
             </div>
           </div>
         </div>
