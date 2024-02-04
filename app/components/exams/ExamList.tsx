84ac1ef813db929c66388922b1afe75e96cb492d
developersung13 <developersung13@gmail.com>
2024-02-04 11:42:45 +0900

    [fix]: {추가된 시험} 리스트 컴포넌트 내 fetching Pending 상태의 표시에 대한 UI 요소 렌더링 코드 추가 (#166)

--- app/components/exams/ExamList.tsx
+++ app/components/exams/ExamList.tsx
@@ -5,6 +5,7 @@ import axiosInstance from '../../utils/axiosInstance';
 import { useQuery } from '@tanstack/react-query';
 import NoneExamListItem from './NoneExamListItem';
 import ExamListItem from './ExamListItem';
+import DummyExamListItem from './DummyExamListItem'; // DummyExamListItem 컴포넌트를 import 해야 합니다.
 import { ExamInfo } from '@/app/types/exam';
 
 // 시험 목록 반환 API (최근 데이터 3개까지)
@@ -15,20 +16,37 @@ const fetchExams = () => {
 };
 
 export default function ExamList() {
-  const { data } = useQuery({
+  const { isPending, data } = useQuery({
     queryKey: ['examList'],
     queryFn: fetchExams,
   });
 
   const resData = data?.data.data;
+  const examCnt = resData?.documents.length || 0;
 
-  if (resData?.documents.length === 0) return <NoneExamListItem />;
+  // DummyExamListItem 컴포넌트를 필요한 만큼 렌더링하기 위한 배열을 생성합니다.
+  const dummyItemsCount = 3 - examCnt;
+  const dummyItems = Array.from({ length: dummyItemsCount }, (_, index) => (
+    <DummyExamListItem key={`dummy-${index}`} />
+  ));
+
+  if (isPending) {
+    const loadingItems = Array.from({ length: 3 }, (_, index) => (
+      <DummyExamListItem key={`loading-${index}`} />
+    ));
+    return <>{loadingItems}</>;
+  }
+
+  if (examCnt === 0) {
+    return <NoneExamListItem />;
+  }
 
   return (
     <>
       {resData?.documents.map((examInfo: ExamInfo) => (
         <ExamListItem examInfo={examInfo} key={examInfo._id} />
       ))}
+      {dummyItems}
     </>
   );
 }
