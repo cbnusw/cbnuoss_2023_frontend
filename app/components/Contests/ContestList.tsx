5766eb3a442091d0946187b8ab49366cb5ca5504
developersung13 <developersung13@gmail.com>
2024-02-07 10:39:46 +0900

    [feat]: 신청 가능한 대회 리스트 표시 로직 수정 (#172)

--- app/components/Contests/ContestList.tsx
+++ app/components/Contests/ContestList.tsx
@@ -15,46 +15,56 @@ const fetchProgressingContests = () => {
 };
 
 export default function ContestList() {
-  const { data } = useQuery({
+  const { isPending, data } = useQuery({
     queryKey: ['progressingContests'],
     queryFn: fetchProgressingContests,
   });
 
-  // 실제 대회 정보를 렌더링한 횟수를 추적
-  let renderedContestsCount = 0;
-
   const resData = data?.data.data;
 
-  if (resData?.documents.length === 0) return <NoneContestListItem />;
+  // Filter contests that are applicable
+  const applicableContests = resData?.documents.filter(
+    (contestInfo: ContestInfo) => {
+      const now = new Date();
+      let isContestApplicable = false;
+
+      if (contestInfo.applyingPeriod) {
+        const applyingPeriodEnd = new Date(contestInfo.applyingPeriod.end);
+        isContestApplicable = applyingPeriodEnd > now;
+      } else {
+        const testPeriodStart = new Date(contestInfo.testPeriod.start);
+        isContestApplicable = testPeriodStart > now;
+      }
+
+      return isContestApplicable;
+    },
+  );
+
+  if (resData?.documents.length === 0 || applicableContests?.length === 0)
+    return <NoneContestListItem />;
 
   return (
     <>
-      {resData?.documents.map((contestInfo: ContestInfo) => {
-        const now = new Date();
-        let isContestApplicable = false;
+      {isPending ? (
+        <>
+          {Array.from({ length: 4 }, (_, index) => (
+            <DummyContestListemItem key={index} />
+          ))}
+        </>
+      ) : (
+        <>
+          {applicableContests.map((contestInfo: ContestInfo) => (
+            <ContestListItem contestInfo={contestInfo} key={contestInfo._id} />
+          ))}
 
-        if (contestInfo.applyingPeriod) {
-          // applyingPeriod이 존재하는 경우, applyingPeriod.end를 Date 객체로 변환
-          const applyingPeriodEnd = new Date(contestInfo.applyingPeriod.end);
-          // 현재 시간이 applyingPeriod.end 이전인지 확인
-          isContestApplicable = applyingPeriodEnd > now;
-        } else {
-          // applyingPeriod이 null인 경우, testPeriod.start를 Date 객체로 변환
-          const testPeriodStart = new Date(contestInfo.testPeriod.start);
-          // 현재 시간이 testPeriod.start 이전인지 확인
-          isContestApplicable = testPeriodStart > now;
-        }
-
-        if (!isContestApplicable) return;
-        renderedContestsCount++;
-        return (
-          <ContestListItem contestInfo={contestInfo} key={contestInfo._id} />
-        );
-      })}
-
-      {Array.from({ length: 4 - renderedContestsCount }, (_, index) => (
-        <DummyContestListemItem key={index} />
-      ))}
+          {Array.from(
+            { length: 4 - applicableContests?.length },
+            (_, index) => (
+              <DummyContestListemItem key={index} />
+            ),
+          )}
+        </>
+      )}
     </>
   );
 }
