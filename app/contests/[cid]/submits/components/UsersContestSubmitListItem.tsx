e8c29dbdc26fbda59ae16e769e1a3f7937876e98
developersung13 <developersung13@gmail.com>
2024-02-11 18:03:17 +0900

    [feat]: {대회 코드 제출 목록 리스트 아이템} 컴포넌트 내 조회 데이터 표시 기능 추가 (#186)

--- app/contests/[cid]/submits/components/UsersContestSubmitListItem.tsx
+++ app/contests/[cid]/submits/components/UsersContestSubmitListItem.tsx
@@ -1,12 +1,23 @@
+import { ContestSubmitInfo } from '@/app/types/contest';
+import { formatDateToYYMMDDHHMM } from '@/app/utils/formatDate';
+import { getCodeSubmitResultTypeDescription } from '@/app/utils/getCodeSubmitResultTypeDescription';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
 interface UsersContestSubmitListItemProps {
+  contestSubmitInfo: ContestSubmitInfo;
   cid: string;
+  total: number;
+  page: number;
+  index: number;
 }
 
 export default function UsersContestSubmitListItem({
+  contestSubmitInfo,
   cid,
+  total,
+  page,
+  index,
 }: UsersContestSubmitListItemProps) {
   const router = useRouter();
 
@@ -14,30 +25,41 @@ export default function UsersContestSubmitListItem({
     <tr
       className="border-b dark:border-gray-700 text-xs text-center cursor-pointer hover:bg-gray-50 focus:bg-gray-50"
       onClick={(e) => {
-        router.push(`/contests/${cid}/submits/${'650af6fe9c2734584192d5fb'}`);
+        router.push(`/contests/${cid}/submits/${contestSubmitInfo._id}`);
       }}
     >
       <th
         scope="row"
         className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
       >
-        1
+        {total - (page - 1) * 10 - index}
       </th>
-      <td className="">충북대학교</td>
-      <td className="">소프트웨어학과</td>
-      <td className="">2020123123</td>
-      <td className="">홍길동</td>
-      <td className="">A+B</td>
-      <td className="text-[#0076C0] font-semibold">정답</td>
+      <td className="">{contestSubmitInfo.user.university}</td>
+      <td className="">{contestSubmitInfo.user.department}</td>
+      <td className="">{contestSubmitInfo.user.no}</td>
+      <td className="">{contestSubmitInfo.user.name}</td>
+      <td className="">{contestSubmitInfo.problem.title}</td>
+      <td
+        className={`${
+          contestSubmitInfo.result.type === 'done'
+            ? 'text-[#0076C0]'
+            : 'text-red-500'
+        } font-semibold`}
+      >
+        {getCodeSubmitResultTypeDescription(contestSubmitInfo.result.type)}
+      </td>
       <td>
-        <span>1527 </span>
-        <span className="ml-[-1px] text-red-500">KB</span>
+        <span>{(contestSubmitInfo.result.memory / 1048576).toFixed(2)} </span>
+        <span className="ml-[-1px] text-red-500">MB</span>
       </td>
       <td className="">
-        <span>64 </span> <span className="ml-[-1px] text-red-500">ms</span>
+        <span>{contestSubmitInfo.result.time} </span>{' '}
+        <span className="ml-[-1px] text-red-500">ms</span>
       </td>
-      <td className="">C++17</td>
-      <td className="">2023.09.26 07:00:00</td>
+      <td className="">{contestSubmitInfo.language}</td>
+      <td className="">
+        {formatDateToYYMMDDHHMM(contestSubmitInfo.createdAt)}
+      </td>
     </tr>
   );
 }
