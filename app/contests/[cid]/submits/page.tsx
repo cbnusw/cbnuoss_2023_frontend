49d3f07192d4c257f97ad0baf163101e265776f0
developersung13 <developersung13@gmail.com>
2024-02-11 18:00:56 +0900

    [feat]: {대회 코드 제출 목록} 페이지 컴포넌트 내 데이터 조회 기능 추가 (#186)

--- app/contests/[cid]/submits/page.tsx
+++ app/contests/[cid]/submits/page.tsx
@@ -11,6 +11,17 @@ import { fetchCurrentUserInfo } from '@/app/utils/fetchCurrentUserInfo';
 import { UserInfo } from '@/app/types/user';
 import { useRouter } from 'next/navigation';
 import { OPERATOR_ROLES } from '@/app/constants/role';
+import axiosInstance from '@/app/utils/axiosInstance';
+import { useQuery } from '@tanstack/react-query';
+import { ContestInfo } from '@/app/types/contest';
+
+// 대회 게시글 정보 조회 API
+const fetchContestDetailInfo = ({ queryKey }: any) => {
+  const cid = queryKey[1];
+  return axiosInstance.get(
+    `${process.env.NEXT_PUBLIC_API_VERSION}/contest/${cid}`,
+  );
+};
 
 interface DefaultProps {
   params: {
@@ -19,27 +30,40 @@ interface DefaultProps {
 }
 
 export default function UsersContestSubmits(props: DefaultProps) {
+  const cid = props.params.cid;
+
+  const { isPending, data } = useQuery({
+    queryKey: ['contestDetailInfo', cid],
+    queryFn: fetchContestDetailInfo,
+  });
+
   const updateUserInfo = userInfoStore((state: any) => state.updateUserInfo);
+
+  const resData = data?.data.data;
+  const contestInfo: ContestInfo = resData;
 
   const [isLoading, setIsLoading] = useState(true);
 
   const router = useRouter();
 
-  const cid = props.params.cid;
-
-  // (로그인 한) 사용자 정보 조회 및 관리자 권한 확인
+  // (로그인 한) 사용자 정보 조회 및 관리자 권한 확인, 그리고 게시글 작성자인지 확인
   useEffect(() => {
     fetchCurrentUserInfo(updateUserInfo).then((userInfo: UserInfo) => {
-      if (userInfo.isAuth && !OPERATOR_ROLES.includes(userInfo.role)) {
-        alert('접근 권한이 없습니다.');
-        router.back();
-        return;
+      if (contestInfo) {
+        if (
+          (userInfo.isAuth && !OPERATOR_ROLES.includes(userInfo.role)) ||
+          userInfo._id !== contestInfo.writer._id
+        ) {
+          alert('접근 권한이 없습니다.');
+          router.back();
+          return;
+        }
+        setIsLoading(false);
       }
-      setIsLoading(false);
     });
-  }, [updateUserInfo, router]);
+  }, [updateUserInfo, contestInfo, router]);
 
-  if (isLoading) return <Loading />;
+  if (isLoading || isPending) return <Loading />;
 
   return (
     <div className="mt-2 px-5 2lg:px-0 overflow-x-auto">
@@ -62,7 +86,7 @@ export default function UsersContestSubmits(props: DefaultProps) {
               href={`/contests/${cid}`}
               className="mt-1 ml-1 text-xl font-medium cursor-pointer hover:underline hover:text-[#0038a8] focus:underline focus:text-[#0038a8] text-[#1048b8]"
             >
-              (대회: 2023년 제2회 충청북도 대학생 프로그래밍 경진대회 본선)
+              ({contestInfo.title})
             </Link>
           </div>
         </p>
@@ -119,147 +143,7 @@ export default function UsersContestSubmits(props: DefaultProps) {
         </div>
 
         <section className="dark:bg-gray-900">
-          <div className="mx-auto w-full">
-            <div className="border dark:bg-gray-800 relative overflow-hidden rounded-sm">
-              <div className="overflow-x-auto">
-                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                  <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400 text-center">
-                    <tr>
-                      <th scope="col" className="w-16 px-4 py-2">
-                        번호
-                      </th>
-                      <th scope="col" className="px-4 py-2">
-                        대학
-                      </th>
-                      <th scope="col" className="px-4 py-2">
-                        학과(부)
-                      </th>
-                      <th scope="col" className="px-4 py-2">
-                        학번
-                      </th>
-                      <th scope="col" className="px-4 py-2">
-                        이름
-                      </th>
-                      <th scope="col" className="px-4 py-2">
-                        문제명
-                      </th>
-                      <th scope="col" className="px-4 py-2">
-                        결과
-                      </th>
-                      <th scope="col" className="px-4 py-2">
-                        메모리
-                      </th>
-                      <th scope="col" className="px-4 py-2">
-                        시간
-                      </th>
-                      <th scope="col" className="px-4 py-2">
-                        언어
-                      </th>
-                      <th scope="col" className="px-4 py-2">
-                        제출 시간
-                      </th>
-                    </tr>
-                  </thead>
-                  <UsersContestSubmitList cid={cid} />
-                </table>
-              </div>
-            </div>
-            <nav
-              className="flex flex-col md:flex-row text-xs justify-between items-start md:items-center space-y-3 md:space-y-0 pl-1 mt-3"
-              aria-label="Table navigation"
-            >
-              <span className="text-gray-500 dark:text-gray-400">
-                <span className="text-gray-500 dark:text-white"> 1 - 10</span>{' '}
-                of
-                <span className="text-gray-500 dark:text-white"> 1000</span>
-              </span>
-              <ul className="inline-flex items-stretch -space-x-px">
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center h-full py-1.5 px-[0.3rem] ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    <span className="sr-only">Previous</span>
-                    <svg
-                      className="w-5 h-5"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-400 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    1
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-400 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    2
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    aria-current="page"
-                    className="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-                  >
-                    3
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-400 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    ...
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-400 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    100
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center h-full py-1.5 px-[0.3rem] leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    <span className="sr-only">Next</span>
-                    <svg
-                      className="w-5 h-5"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
+          <UsersContestSubmitList cid={cid} />
         </section>
       </div>
     </div>
