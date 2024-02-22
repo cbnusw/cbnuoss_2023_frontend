b586ab80e7fe0c5ace40810939fed6b4fcb5d1b1
developersung13 <developersung13@gmail.com>
2024-02-22 22:25:20 +0900

    [feat]: 임시 공지사항 게시글 페이지 컴포넌트 추가 (#212)

---
+++ app/notice/page.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const MarkdownPreview = dynamic(
+  () => import('@uiw/react-markdown-preview').then((mod) => mod.default),
+  { ssr: false },
+);
+
+export default function NoticeTemp() {
+  return (
+    <div className="mt-6 mb-24 px-5 2lg:px-0 overflow-x-auto">
+      <div className="flex flex-col w-[60rem] mx-auto">
+        <div className="flex flex-col gap-3">
+          <p className="text-2xl font-bold tracking-tight">공지사항</p>
+          <div className="flex justify-between border-b border-gray-300"></div>
+        </div>
+        <div className="border-b mt-8 mb-4 pb-5">
+          <MarkdownPreview
+            className="markdown-preview"
+            source={`
+상기 시스템은 현재 <span style="color:hsl(240, 75%, 60%);"><strong>베타 테스트</strong></span> 중에 있습니다.
+
+사용에 있어 궁금하신 사항은 <u>홈페이지 우측 하단의</u>
+
+<b>문의 공간</b>을 이용하여 남겨주세요.
+
+<br />
+
+### 💻 컴파일러 버전 정보
+- **c**: gnu11
+- **c++**: c++17
+- **python2**: 2.7.17
+- **python3**: 3.9.2
+- **openjdk**: 1.8.0_292
+- **kotlin**: 1.5.31-release-548
+- **go**: 1.17.2
+- **node**: 10.24.1
+`}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
