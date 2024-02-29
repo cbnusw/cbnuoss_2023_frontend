d66a5487d5ee45fe4968bd3e5aa61986432a5b2e
developersung13 <developersung13@gmail.com>
2024-02-29 13:44:03 +0900

    [feat]: 미사용 import문 제거 (#224)

--- app/contests/[cid]/submits/[submitId]/page.tsx
+++ app/contests/[cid]/submits/[submitId]/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { OPERATOR_ROLES } from '@/app/constants/role';
 import Loading from '@/app/loading';
 import { userInfoStore } from '@/app/store/UserInfo';
 import { SubmitInfo } from '@/app/types/submit';
