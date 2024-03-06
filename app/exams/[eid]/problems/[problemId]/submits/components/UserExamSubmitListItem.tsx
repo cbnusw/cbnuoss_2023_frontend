0d59d280c453dd4e7fa3df20f60fb0395d744683
developersung13 <developersung13@gmail.com>
2024-03-07 02:29:55 +0900

    [feat]: 시험 문제 코드 제출 목록 페이지 컴포넌트 내 정보 표시 기능 추가 (#242)

--- app/exams/[eid]/problems/[problemId]/submits/components/UserExamSubmitListItem.tsx
+++ app/exams/[eid]/problems/[problemId]/submits/components/UserExamSubmitListItem.tsx
@@ -1,14 +1,23 @@
+import { ExamSubmitInfo } from '@/app/types/exam';
+import { formatDateToYYMMDDHHMM } from '@/app/utils/formatDate';
+import { getCodeSubmitResultTypeDescription } from '@/app/utils/getCodeSubmitResultTypeDescription';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
 interface ExamSubmitListItemProps {
+  personalUserExamSubmitInfo: ExamSubmitInfo;
   eid: string;
   problemId: string;
+  total: number;
+  index: number;
 }
 
 export default function UserExamSubmitListItem({
+  personalUserExamSubmitInfo,
   eid,
   problemId,
+  total,
+  index,
 }: ExamSubmitListItemProps) {
   const router = useRouter();
 
@@ -17,7 +26,7 @@ export default function UserExamSubmitListItem({
       className="border-b dark:border-gray-700 text-xs text-center cursor-pointer hover:bg-gray-50 focus:bg-gray-50"
       onClick={(e) => {
         router.push(
-          `/exams/${eid}/problems/${problemId}/submits/${'65445155c4fc3d9d4396ae0e'}`,
+          `/exams/${eid}/problems/${problemId}/submits/${personalUserExamSubmitInfo._id}`,
         );
       }}
     >
@@ -25,19 +34,34 @@ export default function UserExamSubmitListItem({
         scope="row"
         className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
       >
-        1
+        {total - index}
       </th>
-      <td className="">A+B</td>
-      <td className="text-[#0076C0] font-semibold">정답</td>
+      <td className="">{personalUserExamSubmitInfo.problem.title}</td>
+      <td
+        className={`${
+          personalUserExamSubmitInfo.result?.type === 'done'
+            ? 'text-[#0076C0]'
+            : 'text-red-500'
+        } font-semibold`}
+      >
+        {getCodeSubmitResultTypeDescription(
+          personalUserExamSubmitInfo.result?.type,
+        )}
+      </td>
       <td>
-        <span>1527 </span>
+        <span>
+          {(personalUserExamSubmitInfo.result?.memory / 1048576).toFixed(2)}{' '}
+        </span>
         <span className="ml-[-1px] text-red-500">MB</span>
       </td>
       <td className="">
-        <span>64 </span> <span className="ml-[-1px] text-red-500">ms</span>
+        <span>{personalUserExamSubmitInfo.result.time} </span>{' '}
+        <span className="ml-[-1px] text-red-500">ms</span>
       </td>
-      <td className="">C++17</td>
-      <td className="">2023.09.26 07:00:00</td>
+      <td className="">{personalUserExamSubmitInfo.language}</td>
+      <td className="">
+        {formatDateToYYMMDDHHMM(personalUserExamSubmitInfo.createdAt)}
+      </td>
     </tr>
   );
 }
