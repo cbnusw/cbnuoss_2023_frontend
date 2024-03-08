ddedb552c23d927d54991ae7d7c8c654faf4d06f
developersung13 <developersung13@gmail.com>
2024-03-08 12:08:48 +0900

    [feat]: 개별 제출 정보 컴포넌트 내 정보 표현 기능 수정 (#250)

--- app/contests/[cid]/problems/[problemId]/submits/components/UserContestSubmitListItem.tsx
+++ app/contests/[cid]/problems/[problemId]/submits/components/UserContestSubmitListItem.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import { ContestSubmitInfo } from '@/app/types/contest';
 import { formatDateToYYMMDDHHMM } from '@/app/utils/formatDate';
-import { getCodeSubmitResultTypeDescription } from '@/app/utils/getCodeSubmitResultTypeDescription';
+import {
+  getCodeSubmitResultTypeColor,
+  getCodeSubmitResultTypeDescription,
+} from '@/app/utils/getCodeSubmitResultTypeDescription';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface ContestSubmitListItemProps {
   personalUserContestSubmitInfo: ContestSubmitInfo;
@@ -19,15 +24,26 @@ export default function UserContestSubmitListItem({
   total,
   index,
 }: ContestSubmitListItemProps) {
+  const [loadingDots, setLoadingDots] = useState('');
+
   const router = useRouter();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLoadingDots((prev) => (prev.length < 3 ? prev + '.' : ''));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <tr
       className="border-b dark:border-gray-700 text-xs text-center cursor-pointer hover:bg-gray-50 focus:bg-gray-50"
       onClick={(e) => {
-        router.push(
-          `/contests/${cid}/problems/${problemId}/submits/${personalUserContestSubmitInfo._id}`,
-        );
+        personalUserContestSubmitInfo.result &&
+          router.push(
+            `/contests/${cid}/problems/${problemId}/submits/${personalUserContestSubmitInfo._id}`,
+          );
       }}
     >
       <th
@@ -37,27 +53,42 @@ export default function UserContestSubmitListItem({
         {total - index}
       </th>
       <td className="">{personalUserContestSubmitInfo.problem.title}</td>
-      <td
-        className={`${
-          personalUserContestSubmitInfo.result?.type === 'done'
-            ? 'text-[#0076C0]'
-            : 'text-red-500'
-        } font-semibold`}
-      >
-        {getCodeSubmitResultTypeDescription(
-          personalUserContestSubmitInfo.result?.type,
-        )}
-      </td>
-      <td>
-        <span>
-          {(personalUserContestSubmitInfo.result.memory / 1048576).toFixed(2)}{' '}
-        </span>
-        <span className="ml-[-1px] text-red-500">MB</span>
-      </td>
-      <td className="">
-        <span>{personalUserContestSubmitInfo.result.time} </span>{' '}
-        <span className="ml-[-1px] text-red-500">ms</span>
-      </td>
+      {personalUserContestSubmitInfo.result ? (
+        <>
+          <td
+            className={`text-[${getCodeSubmitResultTypeColor(
+              personalUserContestSubmitInfo.result.type,
+            )}] font-semibold`}
+          >
+            {getCodeSubmitResultTypeDescription(
+              personalUserContestSubmitInfo.result.type,
+            )}
+          </td>
+          <td>
+            <span>
+              {(personalUserContestSubmitInfo.result.memory / 1048576).toFixed(
+                2,
+              )}{' '}
+            </span>
+            <span className="ml-[-1px] text-red-500">MB</span>
+          </td>
+          <td className="">
+            <span>{personalUserContestSubmitInfo.result.time} </span>{' '}
+            <span className="ml-[-1px] text-red-500">ms</span>
+          </td>
+        </>
+      ) : (
+        <>
+          <td className="flex gap-[0.6rem] justify-center items-center w-[3.5rem] h-10 text-[#e67e22] font-semibold mx-auto">
+            채점 중
+            <span className="w-1 ml-[-0.6rem] text-[#e67e22]">
+              {loadingDots}
+            </span>
+          </td>
+          <td>-</td>
+          <td>-</td>
+        </>
+      )}
       <td className="">{personalUserContestSubmitInfo.language}</td>
       <td className="">
         {formatDateToYYMMDDHHMM(personalUserContestSubmitInfo.createdAt)}
