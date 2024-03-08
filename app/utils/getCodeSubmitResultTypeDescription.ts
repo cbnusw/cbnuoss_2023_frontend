0ca98815169c2a5164c86055e2005a2ee109efb5
developersung13 <developersung13@gmail.com>
2024-03-08 12:09:47 +0900

    [feat]: 코드 제출 결과 정보에 따른 hex color 코드 문자열 반환 함수 추가 (#250)

--- app/utils/getCodeSubmitResultTypeDescription.ts
+++ app/utils/getCodeSubmitResultTypeDescription.ts
@@ -1,9 +1,9 @@
 export function getCodeSubmitResultTypeDescription(resultType: string): string {
   switch (resultType) {
     case 'compile':
-      return '컴파일 오류';
+      return '컴파일 에러';
     case 'runtime':
-      return '런타임 오류';
+      return '런타임 에러';
     case 'timeout':
       return '시간 초과';
     case 'memory':
@@ -14,5 +14,24 @@ export function getCodeSubmitResultTypeDescription(resultType: string): string {
       return '정답';
     default:
       return '알 수 없음';
+  }
+}
+
+export function getCodeSubmitResultTypeColor(resultType: string): string {
+  switch (resultType) {
+    case 'compile':
+      return '#0f4c81';
+    case 'runtime':
+      return '#5c4c87';
+    case 'timeout':
+    case 'memory':
+      return '#fa7268';
+    case 'wrong':
+      return '#dd4124';
+    case 'done':
+      return '#009874';
+    default:
+      alert('Error: unknown [resultType]');
+      return 'X';
   }
 }
