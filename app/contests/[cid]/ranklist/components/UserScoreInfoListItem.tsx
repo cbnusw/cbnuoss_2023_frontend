b25b49a387548d6b4a62cd8ccec2926cc25d4a8e
developersung13 <developersung13@gmail.com>
2024-06-28 01:20:00 +0900

    [feat]: 대회 순위 게시글 페이지 모바일 해상도에 대한 반응형 웹 디자인 효과 추가 (#268)

--- app/contests/[cid]/ranklist/components/UserScoreInfoListItem.tsx
+++ app/contests/[cid]/ranklist/components/UserScoreInfoListItem.tsx
@@ -20,10 +20,6 @@ export default function UserScoreInfoListItem({
 }: UserScoreInfoListItemProps) {
   const router = useRouter();
 
-  const handleGoToTheProblem = (problemId: string) => {
-    router.push(`/contests/${cid}/problems/${problemId}`);
-  };
-
   return (
     <div className="flex justify-between min-h-[9rem] border border-gray-300 shadow-md">
       <div className="flex flex-col p-3">
@@ -48,7 +44,7 @@ export default function UserScoreInfoListItem({
                 score.right ? 'bg-[#3870e0]' : 'bg-[#e0e0e0]'
               } border ${
                 score.right ? 'border-[#3565c4]' : 'border-[#d3d3d3]'
-              } hover:brightness-90 duration-150`}
+              }`}
             >
               <div className="absolute top-[0.175rem] left-[0.175rem]">
                 <svg
@@ -61,36 +57,37 @@ export default function UserScoreInfoListItem({
                   <path d="M480.238-137q-71.145 0-133.868-27.023t-109.12-73.348q-46.398-46.325-73.324-108.826Q137-408.699 137-479.762q0-71.145 27.023-133.868t73.348-109.12q46.325-46.398 108.826-73.324Q408.699-823 479.762-823q71.145 0 133.868 27.023t109.12 73.348q46.398 46.325 73.324 108.826Q823-551.301 823-480.238q0 71.145-27.023 133.868t-73.348 109.12q-46.325 46.398-108.826 73.324Q551.301-137 480.238-137Z" />
                 </svg>
               </div>
-              <button
+              <div
                 className={`${
                   score.right ? 'text-white' : 'text-[#3f3f3f]'
-                } text-[0.825rem] underline font-medium hover:text-[#eee] focus:text-[#eee]`}
-                onClick={() => handleGoToTheProblem(score.problem)}
+                } text-[0.825rem] font-medium`}
               >
                 문제 {index + 1}번
-              </button>
-              <span
-                className={`${
-                  score.right ? 'text-white' : 'text-[#3f3f3f]'
-                } text-[0.825rem]`}
-              >
-                시도: {score.tries}회
-              </span>
-              <span
-                className={`${
-                  score.right ? 'text-white' : 'text-[#3f3f3f]'
-                } text-[0.825rem]`}
-              >
-                시간: {score.time}분
-              </span>
+              </div>
+              <div className="mt-1 flex flex-col text-center">
+                <span
+                  className={`${
+                    score.right ? 'text-white' : 'text-[#3f3f3f]'
+                  } text-[0.5rem]`}
+                >
+                  시도: {score.tries}회
+                </span>
+                <span
+                  className={`${
+                    score.right ? 'mt-[-0.2rem] text-white' : 'text-[#3f3f3f]'
+                  } text-[0.5rem]`}
+                >
+                  시간: {score.time}분
+                </span>
+              </div>
             </div>
           ))}
         </div>
       </div>
 
-      <div className="flex justify-around w-32 border-l border-gray-300">
+      <div className="flex justify-around border-l border-gray-300">
         <div className="flex flex-col w-full text-center bg-[#f7f7f7]">
-          <div className="flex justify-center items-center text-[0.825rem] font-semibold bg-[#f7f7f7] h-8 border-b border-gray-300">
+          <div className="w-12 3md:w-[4.5rem] flex justify-center items-center text-[0.825rem] font-semibold bg-[#f7f7f7] h-8 border-b border-gray-300">
             점수
           </div>
           <div className="flex justify-center items-center bg-white h-full">
@@ -98,7 +95,7 @@ export default function UserScoreInfoListItem({
           </div>
         </div>
         <div className="flex flex-col w-full text-center bg-[#f7f7f7]">
-          <div className="flex justify-center items-center text-[0.825rem] font-semibold bg-[#f7f7f7] h-8 border-b border-l border-gray-300">
+          <div className="w-12 3md:w-[4.5rem] flex justify-center items-center text-[0.825rem] font-semibold bg-[#f7f7f7] h-8 border-b border-l border-gray-300">
             패널티
           </div>
           <div className="flex justify-center items-center bg-white h-full border-l text-red-600">
