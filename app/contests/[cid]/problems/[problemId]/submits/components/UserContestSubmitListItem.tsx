d1df35502c3dbd8a771bd939242175c8dc6d3942
developersung13 <developersung13@gmail.com>
2024-06-29 21:05:34 +0900

    [bug] 대회 문제 제출 목록 관련 페이지 내 코드 컴파일 결과 속성의 null인 경우 페이지 로드 오류 제거 (#288)

--- app/contests/[cid]/problems/[problemId]/submits/components/UserContestSubmitListItem.tsx
+++ app/contests/[cid]/problems/[problemId]/submits/components/UserContestSubmitListItem.tsx
@@ -53,42 +53,31 @@ export default function UserContestSubmitListItem({
         {total - index}
       </th>
       <td className="">{personalUserContestSubmitInfo.problem.title}</td>
-      {personalUserContestSubmitInfo.result ? (
-        <>
-          <td
-            className={`${getCodeSubmitResultTypeColor(
+      <td
+        className={`${
+          personalUserContestSubmitInfo.result
+            ? getCodeSubmitResultTypeColor(
+                personalUserContestSubmitInfo.result.type,
+              )
+            : ''
+        } font-semibold`}
+      >
+        {personalUserContestSubmitInfo.result
+          ? getCodeSubmitResultTypeDescription(
               personalUserContestSubmitInfo.result.type,
-            )} font-semibold`}
-          >
-            {getCodeSubmitResultTypeDescription(
-              personalUserContestSubmitInfo.result.type,
-            )}
-          </td>
-          <td>
-            <span>
-              {(personalUserContestSubmitInfo.result.memory / 1048576).toFixed(
-                2,
-              )}{' '}
-            </span>
-            <span className="ml-[-1px] text-red-500">MB</span>
-          </td>
-          <td className="">
-            <span>{personalUserContestSubmitInfo.result.time} </span>{' '}
-            <span className="ml-[-1px] text-red-500">ms</span>
-          </td>
-        </>
-      ) : (
-        <>
-          <td className="flex gap-[0.6rem] justify-center items-center w-[3.5rem] h-10 text-[#e67e22] font-semibold mx-auto">
-            채점 중
-            <span className="w-1 ml-[-0.6rem] text-[#e67e22]">
-              {loadingDots}
-            </span>
-          </td>
-          <td>-</td>
-          <td>-</td>
-        </>
-      )}
+            )
+          : 'N/A'}
+      </td>
+      <td>
+        <span>
+          {(personalUserContestSubmitInfo.result?.memory / 1048576).toFixed(2)}{' '}
+        </span>
+        <span className="ml-[-1px] text-red-500">MB</span>
+      </td>
+      <td className="">
+        <span>{personalUserContestSubmitInfo.result?.time} </span>{' '}
+        <span className="ml-[-1px] text-red-500">ms</span>
+      </td>
       <td className="">{personalUserContestSubmitInfo.language}</td>
       <td className="">
         {formatDateToYYMMDDHHMM(personalUserContestSubmitInfo.createdAt)}
