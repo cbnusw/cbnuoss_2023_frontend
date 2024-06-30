7c960ed45622591f7bdc4177b37ef8a0bcff564d
developersung13 <developersung13@gmail.com>
2024-06-30 11:44:55 +0900

    [fix] 시험 문제 코드 전체 제출목록 내 채점 중 표시 로직 추가 (#292)

--- app/exams/[eid]/submits/components/UsersExamSubmitListItem.tsx
+++ app/exams/[eid]/submits/components/UsersExamSubmitListItem.tsx
@@ -1,8 +1,11 @@
 import { ExamSubmitInfo } from '@/app/types/exam';
 import { formatDateToYYMMDDHHMM } from '@/app/utils/formatDate';
-import { getCodeSubmitResultTypeDescription } from '@/app/utils/getCodeSubmitResultTypeDescription';
+import {
+  getCodeSubmitResultTypeColor,
+  getCodeSubmitResultTypeDescription,
+} from '@/app/utils/getCodeSubmitResultTypeDescription';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface UsersExamSubmitListItemProps {
   examSubmitInfo: ExamSubmitInfo;
@@ -19,13 +22,24 @@ export default function UsersExamSubmitListItem({
   page,
   index,
 }: UsersExamSubmitListItemProps) {
+  const [loadingDots, setLoadingDots] = useState('');
+
   const router = useRouter();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLoadingDots((prev) => (prev.length < 3 ? prev + '.' : ''));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <tr
       className="border-b dark:border-gray-700 text-xs text-center cursor-pointer hover:bg-gray-50 focus:bg-gray-50"
       onClick={(e) => {
-        router.push(`/exams/${eid}/submits/${examSubmitInfo._id}`);
+        examSubmitInfo.result &&
+          router.push(`/exams/${eid}/submits/${examSubmitInfo._id}`);
       }}
     >
       <th
@@ -37,23 +51,36 @@ export default function UsersExamSubmitListItem({
       <td className="">{examSubmitInfo.user.no}</td>
       <td className="">{examSubmitInfo.user.name}</td>
       <td className="">{examSubmitInfo.problem.title}</td>
-      <td
-        className={`${
-          examSubmitInfo.result.type === 'done'
-            ? 'text-[#0076C0]'
-            : 'text-red-500'
-        } font-semibold`}
-      >
-        {getCodeSubmitResultTypeDescription(examSubmitInfo.result.type)}
-      </td>
-      <td>
-        <span>{(examSubmitInfo.result.memory / 1048576).toFixed(2)} </span>
-        <span className="ml-[-1px] text-red-500">MB</span>
-      </td>
-      <td className="">
-        <span>{examSubmitInfo.result.time} </span>{' '}
-        <span className="ml-[-1px] text-red-500">ms</span>
-      </td>
+      {examSubmitInfo.result ? (
+        <>
+          <td
+            className={`${getCodeSubmitResultTypeColor(
+              examSubmitInfo.result.type,
+            )} font-semibold`}
+          >
+            {getCodeSubmitResultTypeDescription(examSubmitInfo.result.type)}
+          </td>
+          <td>
+            <span>{(examSubmitInfo.result.memory / 1048576).toFixed(2)} </span>
+            <span className="ml-[-1px] text-red-500">MB</span>
+          </td>
+          <td className="">
+            <span>{examSubmitInfo.result.time} </span>{' '}
+            <span className="ml-[-1px] text-red-500">ms</span>
+          </td>
+        </>
+      ) : (
+        <>
+          <td className="flex gap-[0.6rem] justify-center items-center w-[3.5rem] h-10 text-[#e67e22] font-semibold mx-auto">
+            채점 중
+            <span className="w-1 ml-[-0.6rem] text-[#e67e22]">
+              {loadingDots}
+            </span>
+          </td>
+          <td>-</td>
+          <td>-</td>
+        </>
+      )}
       <td className="">{examSubmitInfo.language}</td>
       <td className="">{formatDateToYYMMDDHHMM(examSubmitInfo.createdAt)}</td>
     </tr>
