c4d0b0b5c0a695010149b226adfb6340470fbac2
developersung13 <developersung13@gmail.com>
2024-09-04 17:55:19 +0900

    feat: 공지사항 목록 페이지 구성 컴포넌트 추가

--- app/notices/components/NoticeListItem.tsx
+++ app/notices/components/NoticeListItem.tsx
@@ -1,29 +1,42 @@
-'use client';
-
+import { NoticeInfo } from '@/app/types/notice';
+import {
+  formatDateToYYMMDDHHMM,
+  formatDateToYYMMDD,
+  formatDateToYYMMDDHHMMSS,
+} from '@/app/utils/formatDate';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
-export default function NoticeListItem() {
+interface NoticeListItemProps {
+  noticeInfo: NoticeInfo;
+  total: number;
+  page: number;
+  index: number;
+}
+
+export default function NoticeListItem(props: NoticeListItemProps) {
+  const { noticeInfo, total, page, index } = props;
+
   const router = useRouter();
 
   return (
     <tr
       className="border-b dark:border-gray-700 text-xs text-center cursor-pointer hover:bg-gray-50 focus:bg-gray-50"
       onClick={(e) => {
-        router.push('/notices/64a52966a1203700203b8ddd');
+        router.push(`/notices/${noticeInfo._id}`);
       }}
     >
       <th
         scope="row"
         className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
       >
-        1
+        {total - (page - 1) * 10 - index}
       </th>
-      <td className="hover:underline focus:underline">
-        시스템 이용 간 유의사항
+      <td className="hover:underline focus:underline">{noticeInfo.title}</td>
+      <td className="font-medium">{noticeInfo.writer.name}</td>
+      <td className="font-medium">
+        {formatDateToYYMMDDHHMMSS(noticeInfo.createdAt)}
       </td>
-      <td className="font-medium">신재혁</td>
-      <td className="font-medium">2023.06.26</td>
     </tr>
   );
 }
